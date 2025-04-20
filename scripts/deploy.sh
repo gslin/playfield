4b4b816c42437292d8c65bd8ccce2050b6675bff
Gea-Suan Lin <gslin@gslin.org>
2025-04-20 21:07:09 +0800

    feat: use pypy3.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,11 @@
 
 set -eou pipefail
 
+# pypy
+rm -fr .venv
+mise exec uv@latest -- uv venv --python pypy3
+
+# systemd
 mkdir -p ~/.config/systemd/user || true
 cp playfield.service ~/.config/systemd/user/playfield.service
 systemctl --user daemon-reload
