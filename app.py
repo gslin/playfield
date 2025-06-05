d5915a86188200522340db51cfee7827b6642943
Gea-Suan Lin <gslin@gslin.org>
2025-06-05 23:24:36 +0800

    feat: use APScheduler.

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
+
+def ActiveFitnessJob():
+    pass
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(ActiveFitnessJob, 'interval', minutes=1)
+scheduler.start()
 
 app = Flask(__name__)
 
