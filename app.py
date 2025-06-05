fed9071ac60afca4affeeb9b5802bc7ef3e91123
Gea-Suan Lin <gslin@gslin.org>
2025-06-06 00:15:19 +0800

    feat: remove logger.

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 
 import json
-import logging
 import requests
-
-logger = logging.getLogger(__name__)
 
 class MyJobs:
     vault_json = '/srv/vault/playfield/default.current.json'
@@ -18,8 +15,6 @@ class MyJobs:
             self.vault = json.load(f)
 
     def ActiveFitnessJob(self):
-        logger.debug('ActiveFitnessJob called')
-
         api_secret = self.vault['ACTIVE_FITNESS_API_SECRET']
         api_seed = self.vault['ACTIVE_FITNESS_API_SEED']
         api_currentcount_url = self.vault['ACTIVE_FITNESS_API_CURRENTCOUNT_URL']
